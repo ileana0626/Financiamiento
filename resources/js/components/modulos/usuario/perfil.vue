dd50d8fb3161421523d0f91fa9b2b478f0e040fe
RicardoCorderoR <rcordero954@gmail.com>
2024-09-13 09:37:01 -0600

    Plantilla de formulario para cambio de contraseña añadida en 'Mi Peril'

--- resources/js/components/modulos/usuario/perfil.vue
+++ resources/js/components/modulos/usuario/perfil.vue
@@ -7,7 +7,7 @@
                     <ul class="breadcrumb">
                         <li>
                             <router-link to="/"><span
-                                    class="material-symbols-rounded v-align-icon-bc">home</span></router-link>
+                                class="material-symbols-rounded v-align-icon-bc">home</span></router-link>
                         </li>
                         <li class="breadActive">
                             <span>Mi Perfil</span>
@@ -28,7 +28,7 @@
                                     <img src="/img/user_default.svg" alt="Foto de perfil">
                                 </div>
                             </div>
-                            <div class="col-12 d-flex justify-content-center pb-1">
+                            <div class="col-12 d-flex justify-content-center pb-2">
                                 <span class="nametag-perfil">Nombre Nombre Apellido Apellido</span>
                             </div>
                             <div class="col-12 d-flex justify-content-center">
@@ -55,11 +55,52 @@
                     <div class="edit-btn-perfil">
                         <vs-button block :color="!!(darkMode) ? '#f5f5f5' : '#595959'" :key="'edit'+darkMode">
                             <div style="color: var(--btn-txt-color); font-weight: 700;">
-                                <i class="fas fa-pencil-alt pr-2" style="font-size: 0.8125rem !important;"></i>Editar
+                                <i class="fas fa-user-edit pr-2" style="font-size: 0.8125rem !important;"></i>Editar
                             </div>
                         </vs-button>
                     </div>
                 </div>
+                <div class="row pt-4">
+                    <div class="col-12 col-md-6 card-info">
+                        <div class="card-header d-flex">
+                            <h3 class="card-title font-weight-bold">Actualizar contraseña</h3>
+                        </div>
+                        <div class="card-body container-fluid" style="background-color: var(--iee-white);">
+                            <div class="row">
+                                <div class="col-12 col-xl-10 px-3 pb-3">
+                                    <label class="col-form-label">Contraseña actual</label>
+                                    <vs-input id="contrasenaActual" type="password" color="#C2B280" icon-after
+                                        placeholder="Contraseña actual" autocomplete="off">
+                                    </vs-input>
+                                </div>
+                                <div class="col-12 col-xl-6 px-3 pb-3">
+                                    <label class="col-form-label">Nueva contraseña</label>
+                                    <vs-input id="contrasenaNueva" type="password" color="#C2B280" icon-after
+                                        placeholder="Nueva contraseña" autocomplete="off">
+                                    </vs-input>
+                                </div>
+                                <div class="col-12 col-xl-6 px-3 pb-3">
+                                    <label class="col-form-label">Confirmar contraseña</label>
+                                    <vs-input id="contrasenaConfirma" type="password" color="#C2B280" icon-after
+                                        placeholder="Confirmar contraseña" autocomplete="off">
+                                    </vs-input>
+                                </div>
+                                <div class="col-12 px-3 d-flex justify-content-center">
+                                    <vs-button :color="!!(darkMode) ? '#f5f5f5' : '#595959'" :key="'pass'+darkMode">
+                                        <div style="color: var(--btn-txt-color); font-weight: 700;">
+                                            <i class="fas fa-eraser pr-2" style="font-size: 0.8125rem !important;"></i>Limpiar
+                                        </div>
+                                    </vs-button>      
+                                    <vs-button :color="!!(darkMode) ? '#f5f5f5' : '#595959'" :key="'pass'+darkMode">
+                                        <div style="color: var(--btn-txt-color); font-weight: 700;">
+                                            <i class="fas fa-pencil-alt pr-2" style="font-size: 0.8125rem !important;"></i>Actualizar
+                                        </div>
+                                    </vs-button>                                
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                </div>            
             </div>
         </div>
     </div>
