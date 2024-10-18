03e8558915aace986fcf751b2b59cce370db7a6d
RicardoCorderoR <rcordero954@gmail.com>
2024-10-18 16:19:38 -0600

    Diseño de tarjeta 'Cohete' elegida para dahsboard - Se cambio la imagen del cohete - Se ajustaron los estilos de la imagen de fondo - Se mejoro la responsividad general del dashboard

--- resources/js/components/modulos/dashboard/index.vue
+++ resources/js/components/modulos/dashboard/index.vue
@@ -1,58 +1,17 @@
 <template>
     <div class="content-header">
-        <div id="contentRegistro" class="content container-fluid h-auto mb-5 mb-sm-0">
-            <div class="container-fluid px-3 px-xl-5 py-3 mt-3">
-                <div class="row overflow-hidden">
-                    <div class="col-12 col-md-6 col-xl-6 d-flex justify-content-start izq-row pl-4 pl-md-0" style="background-color: #CC3843; border-radius: 25px;" >
-                        <img src="/img/LOGO_NUEVO.png" alt="" class="bg-left-img" style="height: auto;">
-                    </div>
-                    <div class="col-12 col-md-8 col-lg-6 offset-md-2 pt-5 pb-3 mq-pos mt-md-3" style="background-color: #0c1f2c; color: white;">
-                        <div class="container-fluid pt-5 pt-md-0 der-row">
-                            <div class="bg-right-img bg-white">
-                                <img draggable="false" src="/img/LOGO_NUEVO.png" alt="Foto de perfil" class="bg-right-img-adjust">
-                            </div>
-                            <div class="offset-0 offset-md-3 p-3">
-                                <h1 class="font-weight-bold pt-3 pt-md-0">{{ `¡Buen día ${cfullname}!` }}</h1>
-                                <p class="text-justify mt-3 font-weight-bold">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur veritatis aliquid incidunt sapiente doloribus 
-                                    distinctio voluptatibus illum natus minima laborum quos in fugiat, pariatur architecto animi saepe esse id repellendus?
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>             
+        <div id="contentRegistro" class="content container-fluid h-auto mb-5 mb-sm-0">            
             <div class="row">
-                <div class="col-sm-6 mx-auto">
-                    <!-- <div class="card cardintro d-flex flex-row" style="height: 19rem">
-                        <div class="tituloCard flex-grow-1 p-3 align-self-center" style="color: var(--iee-white);">
-                            <h5 class="font-weight-bold tituloin">
-                                ¡Buen día {{ cfullname }}!
-                            </h5>
-                            <h6 class="subtituloin font-weight-bold subtituloin">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi odit facilis neque
-                                corporis laborum sit, ratione consectetur officiis quae, itaque deleniti ullam amet
-                                soluta labore repellat, expedita numquam assumenda impedit.
-                            </h6>
-                        </div>
-                        <div class="align-self-center">
-                            <img src="/img/imglogro.webp" style="height: 10vh !important;">
-                        </div>
-                    </div> -->
-                    <!-- <div class="col ">
-                        <div class="row mt-4 rowvalidados">
-                        </div>
-                    </div> -->
-
-                    <div class="container-fluid">
+                <div class="col-12">
+                    <div class="container-fluid px-3 px-md-5">
                         <div class="card p-3 mt-5 position-relative sky-card" :class="!!darkMode ? 'shadow-lg-dark' : 'shadow'">
-                            <div class="top-bg" style="background-image: url('/img/sky-bg-card.png');"></div>
+                            <div class="top-bg" style="background-image: url('/img/sky-bg-card.png')"></div>
                             <div class="py-5">
                                 <img src="/img/rocket-bg-card.png" alt="" class="rocket-img">
                             </div>
                             <div style="z-index: 2;" class="pt-5 mt-3">
-                                <h1 class="text-center font-weight-bold">{{ `¡Buen día ${cfullname}!` }}</h1>
-                                <p class="text-justify font-weight-bold p-2">
+                                <h1 class="text-center text-md-left font-weight-bold px-1">{{ `¡Buen día ${cfullname}!` }}</h1>
+                                <p class="text-justify font-weight-bold p-2" style="color: #595959;">
                                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, facere explicabo! Culpa in debitis qui, 
                                     praesentium, ea facilis quidem earum non recusandae iste sequi blanditiis animi voluptatibus iure consequuntur deserunt.
                                 </p>
@@ -60,7 +19,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-5 mx-auto">
+            </div>            
+            <div class="row px-3 px-md-5 mt-4">
+                <div class="col-12 col-xl-6">
                     <vs-table>
                         <template #thead>
                             <vs-tr>
@@ -93,26 +54,6 @@
                                 style="background-color: var(--iee-white) !important;" />
                         </template>
                     </vs-table>
-                </div>
-            </div>
-            <div class="container-fluid px-3 px-xl-5 py-3" style="height: auto;">
-                <div class="dash-cloud-card shadow">
-                    <div class="clouds"></div>
-                    <div class="row p-5">
-                        <div class="col-12 col-lg-7 font-weight-bold">
-                            <h1 class="font-weight-bold" style="color: #541FAB;">Bienvenido, {{ cfullname }}</h1>
-                            <br>
-                            <p class="text-justify" style="color: #595959">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam rem nisi atque, veniam quaerat 
-                                voluptas laudantium delectus beatae similique provident quo ratione deleniti recusandae in quos aperiam sequi quibusdam cumque.
-                            </p>
-                        </div>
-                        <div class="col">
-                            <div class="ribbon">
-                                <img src="/img/ribbon-bg.png" alt="liston banner bienvenida">
-                            </div>                            
-                        </div>
-                    </div>
                 </div>
             </div>
             <!-- <div class="col-11 card fechasPeriodos mx-auto my-4"
