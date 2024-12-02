3e80effce856515a725971c6ea81a280a61ba622
RicardoCorderoR <rcordero954@gmail.com>
2024-12-02 09:40:03 -0600

    Versión actualizada - 1.18.34.29112024

--- resources/js/components/plantilla/Footer.vue
+++ resources/js/components/plantilla/Footer.vue
@@ -12,7 +12,7 @@
                     Informática</small>
             </div>
             <div class="d-sm-block ml-2 text-right text-sm-center">
-                <small>Versión 1.17.33.28112024</small>
+                <small>Versión 1.18.34.29112024</small>
             </div>
         </div>
     </footer>
