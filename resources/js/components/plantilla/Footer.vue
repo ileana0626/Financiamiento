05f5d7b80a0b2d92e06aa1a6d951d86df8286ea7
RicardoCorderoR <rcordero954@gmail.com>
2024-12-11 09:44:47 -0600

    versión actualizada - 1.20.41.10122024

--- resources/js/components/plantilla/Footer.vue
+++ resources/js/components/plantilla/Footer.vue
@@ -12,7 +12,7 @@
                     Informática</small>
             </div>
             <div class="d-sm-block ml-2 text-right text-sm-center">
-                <small>Versión 1.20.40.09122024</small>
+                <small>Versión 1.20.41.10122024</small>
             </div>
         </div>
     </footer>
